d55a3955fe2c796253cb1d6661b118d9503f3ece
egnuyuki <n48691412921@gmail.com>
2023-09-27 23:18:30 +0900

    slider復活

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Header from '@/components/Header'
 import TwitterTimeline from '@/components/TwitterTimeline'
-// import Slider from '@/components/Slider'
+import Slider from '@/components/Slider'
 import FirstView from '@/components/FirstView'
 import Introduction from '@/components/Introduction'
 import Welcome from '@/components/Welcome'
@@ -11,7 +11,7 @@ export default function Home() {
     <div className=''>
       <main className="flex min-h-screen flex-col items-center justify-between py-20 md:py-10 lg:px-24">
         <Header/>
-        {/* <Slider/> */}
+        <Slider/>
         <Welcome/>
         <Introduction/>
       </main>
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,123 +1,123 @@
-// import Image from 'next/image'
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
+import Image from 'next/image'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
 
-// const ImgDate = [
+const ImgDate = [
 
-//     {
-//         src:"/25th-high.png",
-//         alt:"25th画像高解像",
-//     },
-//     {
-//         src:"/fv-25th.jpg",
-//         alt:"25th画像",
-//     },
-//     {
-//         src:"/fv_red.jpg",
-//         alt:"舞台赤",
-//     },
-//     {
-//         src:"/fv_black.jpg",
-//         alt:"舞台黒",
-//     },
-//     {
-//         src:"/fv_light.jpg",
-//         alt:"舞台ライティング",
-//     },
-//     {
-//         src:"/fv_orange.jpg",
-//         alt:"舞台オレンジ",
-//     },
-// ]
+    {
+        src:"/25th-high.png",
+        alt:"25th画像高解像",
+    },
+    {
+        src:"/fv-25th.jpg",
+        alt:"25th画像",
+    },
+    {
+        src:"/fv_red.jpg",
+        alt:"舞台赤",
+    },
+    {
+        src:"/fv_black.jpg",
+        alt:"舞台黒",
+    },
+    {
+        src:"/fv_light.jpg",
+        alt:"舞台ライティング",
+    },
+    {
+        src:"/fv_orange.jpg",
+        alt:"舞台オレンジ",
+    },
+]
 
 
-// export default function Slider () {
+export default function Slider () {
 
-//     return (
-//         <div className="max-w-screen-2xl w-full mx-auto">
-//             <div className=''>
-//                 <Swiper
-//                 // install Swiper modules
-//                 modules={[Navigation, Pagination, Scrollbar, A11y, ]}
-//                 // spaceBetween={50}
-//                 autoplay = {{
-//                     delay:2000
-//                 }}
-//                 slidesPerView={1}
-//                 loop = {true} 
-//                 navigation
-//                 pagination={{ clickable: true }}
-//                 scrollbar={{ draggable: true }}
-//                 // onSwiper={(swiper) => console.log(swiper)}
-//                 onSlideChange={() => console.log('slide change')}
-//                 >
+    return (
+        <div className="max-w-screen-2xl w-full mx-auto">
+            <div className=''>
+                <Swiper
+                // install Swiper modules
+                modules={[Navigation, Pagination, Scrollbar, A11y, ]}
+                // spaceBetween={50}
+                autoplay = {{
+                    delay:2000
+                }}
+                slidesPerView={1}
+                loop = {true} 
+                navigation
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+                // onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+                >
 
                     
-//                     <SwiperSlide className="">
-//                         <Image
-//                             src="/25th-high.jpg"
-//                             alt="25thImage"
-//                             className="firstview"
-//                             width={1440}
-//                             height={50}
-//                             priority
-//                         />
-//                     </SwiperSlide>
-//                     {/* <SwiperSlide className="">
-//                         <Image
-//                             src="/25th-web.svg"
-//                             alt="25thImage"
-//                             className="firstview"
-//                             width={650}
-//                             height={650}
-//                             priority
-//                         />
-//                     </SwiperSlide> */}
-//                     <SwiperSlide className="">
-//                         <Image
-//                             src="/fv_red.jpg"
-//                             alt="25thImage"
-//                             className="firstview"
-//                             width={1440}
-//                             height={50}
-//                             priority
-//                         />
-//                     </SwiperSlide>
-//                     <SwiperSlide className="">
-//                         <Image
-//                             src="/fv_black.jpg"
-//                             alt="25thImage"
-//                             className="firstview"
-//                             width={1440}
-//                             height={50}
-//                             priority
-//                         />
-//                     </SwiperSlide>
-//                     <SwiperSlide className="">
-//                         <Image
-//                             src="/fv_orange.jpg"
-//                             alt="25thImage"
-//                             className="firstview"
-//                             width={1440}
-//                             height={50}
-//                             priority
-//                         />
-//                     </SwiperSlide>
-//                     <SwiperSlide className="">
-//                         <Image
-//                             src="/fv_light.jpg"
-//                             alt="25thImage"
-//                             className="firstview"
-//                             width={1440}
-//                             height={50}
-//                             priority
-//                         />
-//                     </SwiperSlide>
-//                 </Swiper>
-//             </div>
-//         </div>
-//     )
-// }
+                    <SwiperSlide className="">
+                        <Image
+                            src="/25th-high.jpg"
+                            alt="25thImage"
+                            className="firstview"
+                            width={1440}
+                            height={50}
+                            priority
+                        />
+                    </SwiperSlide>
+                    {/* <SwiperSlide className="">
+                        <Image
+                            src="/25th-web.svg"
+                            alt="25thImage"
+                            className="firstview"
+                            width={650}
+                            height={650}
+                            priority
+                        />
+                    </SwiperSlide> */}
+                    <SwiperSlide className="">
+                        <Image
+                            src="/fv_red.jpg"
+                            alt="25thImage"
+                            className="firstview"
+                            width={1440}
+                            height={50}
+                            priority
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide className="">
+                        <Image
+                            src="/fv_black.jpg"
+                            alt="25thImage"
+                            className="firstview"
+                            width={1440}
+                            height={50}
+                            priority
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide className="">
+                        <Image
+                            src="/fv_orange.jpg"
+                            alt="25thImage"
+                            className="firstview"
+                            width={1440}
+                            height={50}
+                            priority
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide className="">
+                        <Image
+                            src="/fv_light.jpg"
+                            alt="25thImage"
+                            className="firstview"
+                            width={1440}
+                            height={50}
+                            priority
+                        />
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+        </div>
+    )
+}
