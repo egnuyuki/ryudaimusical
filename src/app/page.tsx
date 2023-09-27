ba2bcede53d98f95cf994876eabe6c639426ca43
egnuyuki <n48691412921@gmail.com>
2023-09-27 23:46:25 +0900

    titleタグ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,14 @@ import Slider from '@/components/Slider'
 import FirstView from '@/components/FirstView'
 import Introduction from '@/components/Introduction'
 import Welcome from '@/components/Welcome'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <div className=''>
+      <Head>
+        <title>琉大ミュージカル</title>
+      </Head>
       <main className="flex min-h-screen flex-col items-center justify-between py-20 md:py-10 lg:px-24">
         <Header/>
         <Slider/>
