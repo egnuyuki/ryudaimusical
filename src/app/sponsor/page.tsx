8cba00adefc17a7a4b6fdd52ee86793fdf5ecffb
bluestyle-uehara <shangyuannaoyuki2@gmail.com>
2023-10-11 18:18:24 +0900

    画像軽量化

--- src/app/sponsor/page.tsx
+++ src/app/sponsor/page.tsx
@@ -5,6 +5,77 @@ import Header from '@/components/Header'
 import Record from '@/components/Record '
 import H2 from '@/components/H2'
 
+const Date = [
+  {
+      No:1,
+      src:"/images/sponsors/sponsor1.jpg",
+  },
+  {
+      No:2,
+      src:"/images/sponsors/sponsor2.jpg",
+  },
+  {
+      No:3,
+      src:"/images/sponsors/sponsor3.jpg",
+  },
+  {
+      No:4,
+      src:"/images/sponsors/sponsor4.jpg",
+  },
+  {
+      No:5,
+      src:"/images/sponsors/sponsor5.jpg",
+  },
+  {
+      No:6,
+      src:"/images/sponsors/sponsor6.jpg",
+  },
+  {
+      No:7,
+      src:"/images/sponsors/sponsor7.jpg",
+  },
+  {
+      No:8,
+      src:"/images/sponsors/sponsor8.jpg",
+  },
+  {
+      No:9,
+      src:"/images/sponsors/sponsor9.jpg",
+  },
+  {
+      No:10,
+      src:"/images/sponsors/sponsor10.jpg",
+  },
+  {
+      No:11,
+      src:"/images/sponsors/sponsor11.jpg",
+  },
+  {
+      No:12,
+      src:"/images/sponsors/sponsor12.jpg",
+  },
+  {
+      No:13,
+      src:"/images/sponsors/sponsor13.jpg",
+  },
+  {
+      No:14,
+      src:"/images/sponsors/sponsor14.jpg",
+  },
+  {
+      No:15,
+      src:"/images/sponsors/sponsor15.jpg",
+  },
+  {
+      No:16,
+      src:"/images/sponsors/sponsor16.jpg",
+  },
+  {
+      No:17,
+      src:"/images/sponsors/sponsor17.jpg",
+  },
+]
+
 
 export default function Home() {
   return (
@@ -13,152 +84,20 @@ export default function Home() {
         <Header/>
         <div className="w-full md:w-10/12 mx-auto my-10 px-4 py-8 flex justify-center flex-col  max-w-screen-xl" id="sponsors">
             <H2 h2J='協賛企業' h2E='Sponsors'/>
-            <div className="w-full mt-10">
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685854_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685856_0.jpg" alt=""/>
-                  </div>
-                </div>
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685857_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685858_0.jpg" alt=""/>
-                  </div>
-                </div>
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685859_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685860_0.jpg" alt=""/>
-                  </div>
-                </div>
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685861_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685862_0.jpg" alt=""/>
-                  </div>
-                </div>
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685863_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685864_0.jpg" alt=""/>
-                  </div>
-                </div>
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685865_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685867_0.jpg" alt=""/>
-                  </div>
-                </div>
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685868_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685869_0.jpg" alt=""/>
-                  </div>
-                </div>
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685870_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685871_0.jpg" alt=""/>
-                  </div>
-                </div>
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685872_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685873_0.jpg" alt=""/>
-                  </div>
-                </div>
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685874_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685875_0.jpg" alt=""/>
-                  </div>
-                </div>
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685876_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685878_0.jpg" alt=""/>
-                  </div>
-                </div>
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685879_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685880_0.jpg" alt=""/>
-                  </div>
-                </div>
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685881_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685882_0.jpg" alt=""/>
-                  </div>
-                </div>
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685883_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685884_0.jpg" alt=""/>
-                  </div>
-                </div>
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685885_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685886_0.jpg" alt=""/>
-                  </div>
-                </div>
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685887_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685889_0.jpg" alt=""/>
-                  </div>
-                </div>
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685890_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685891_0.jpg" alt=""/>
-                  </div>
-                </div>
-                <div className="flex w-full flex-shrink">
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685892_0.jpg" alt=""/>
-                  </div>
-                  <div>
-                    <img className='w-full' src="images/sponsors/2022_panfS__4685893_0.jpg" alt=""/>
-                  </div>
-                </div>
-            </div>
+            {Date.map(date => {
+                    return (
+                    <div key = {date.No} className="w-full mt-10 flex justify-center">
+                            <Image
+                            src={date.src}
+                            alt="25thImage"
+                            className="rouded-full"
+                            width={800}
+                            height={650}
+                            priority
+                            />
+                    </div>
+                            )
+                        })}
         </div>
       </main>
     </div>
