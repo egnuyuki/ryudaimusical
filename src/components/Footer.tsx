62e79863ad46072d0141c364d8f94523886fac71
bluestyle-uehara <shangyuannaoyuki2@gmail.com>
2023-10-11 18:49:27 +0900

    ナビ背景

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,8 +29,8 @@ import Link from "next/link";
 import Image from "next/image";
 export default function Footer() {
     return (
-            <div className="w-80 h-14 px-10 py-10 justify-center items-center inline-flex">
-                <div className="h-10 px-5 justify-center items-center gap-8 inline-flex">
+            <div className="w-80 h-20 px-10 py-10 justify-center items-center inline-flex md:h-auto border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800">
+                <div className="h-10 px-5 justify-center items-center gap-8 inline-flex ">
                     {Date.map(date => {
                     return (
                         <div key = {date.text} className="">
