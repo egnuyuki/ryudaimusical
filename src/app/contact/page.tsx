61936ce4fa5548769b04bff77f8c4e5abf3f66b7
bluestyle-uehara <shangyuannaoyuki2@gmail.com>
2023-10-11 18:58:25 +0900

    コンタクト w-max

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <div>
       <main className="flex min-h-screen flex-col items-center justify-between py-20 md:py-0 lg:px-24">
         <Header/>
-        <div className="px-5 my-5 text-2xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="text-2xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-max">
           <p>
             琉大ミュージカル広報
           </p>
