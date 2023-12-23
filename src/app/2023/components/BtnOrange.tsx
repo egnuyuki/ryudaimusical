a15a6c6d6ae408da496c4ed6814af1848df6e27e
bluestyle-uehara <shangyuannaoyuki2@gmail.com>
2023-12-23 13:56:01 +0900

    あか

--- src/app/2023/components/BtnOrange.tsx
+++ src/app/2023/components/BtnOrange.tsx
@@ -8,7 +8,7 @@ type Props = {
   export default function BtnOrange({Href, Text}:Props) {
       return (
         <div className='flex justify-center'>
-          <Link className="p-4 shadow justify-center items-center flex w-fit text-center" href={Href}>
+          <Link className="p-4 shadow justify-center items-center flex w-fit text-center" href={Href} target='brank'>
             <div className="bg-gradient-to-r from-yellow-400 to-orange-400 text-white text-lg font-semibold py-4 px-6 rounded-lg shadow-md hover:shadow-sm hover:translate-y-1 transform transition-all duration-300 text-l font-['Inter']">{Text}</div>
           </Link>
         </div>
