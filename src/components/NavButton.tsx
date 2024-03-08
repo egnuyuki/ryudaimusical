9bc9226de33a935c3902c604e753789f919c8e0f
bluestyle-uehara <shangyuannaoyuki2@gmail.com>
2024-03-08 21:18:40 +0900

    ナビボタン回復

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -6,7 +6,7 @@ export default function NavButton () {
 
     return (
         <div className="w-full fixed z-20 bottom-8 left-0 flex gap-4 items-end justify-center">
-            <div className="w-12 h-12 bg-zinc-100 rounded-full shadow translate-y-2 btn">
+            <div className="w-12 h-12 bg-zinc-100 rounded-full shadow translate-y-2">
                 <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                     <Link className="" href="/" target=''>
                         <Image
@@ -17,10 +17,10 @@ export default function NavButton () {
                             height={20}
                             priority
                         />
-                    </Link> 
+                    </Link>  
                 </div>
             </div>
-            <div className="relative w-12 h-12 bg-zinc-100 rounded-full shadow btn">
+            <div className="w-12 h-12 bg-zinc-100 rounded-full shadow relative">
                 <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                     <Link className="" href="/contact" target=''>
                         <Image
@@ -35,7 +35,7 @@ export default function NavButton () {
                 </div>
             </div>
             <CircleNav/>
-            <div className="w-12 h-12 bg-zinc-100 rounded-full shadow relative btn">
+            <div className="w-12 h-12 bg-zinc-100 rounded-full shadow relative">
                 <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                     <Link className="" href="https://www.instagram.com/ryudaimusical/" target='brank'>
                         <Image
